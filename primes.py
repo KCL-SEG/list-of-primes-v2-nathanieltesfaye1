07ff841be45e60657f175c8f293d88272343174b
Nathaniel Tesfaye <nathaniel.tesfaye@kcl.ac.uk>
2022-09-30 15:01:13 +0100

    Add ValueError when a non-positive parameter is passed into method

--- primes.py
+++ primes.py
@@ -3,4 +3,19 @@
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes <= 0:
+        raise ValueError(f'{number_of_primes} is <= 0. You must input a number > 0.')
+
+    number = 2
+
+    while len(list) != number_of_primes:
+        for i in range(2, number):
+            if number % i == 0:
+                break
+        else:
+            list.append(number)
+
+        number += 1
+
     return list
